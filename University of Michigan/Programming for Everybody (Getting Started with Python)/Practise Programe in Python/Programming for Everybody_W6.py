d5cb3d4f5804ad33b2a36b623aaedd45f8bbbfc5
ManSzeLam109021153 <116985515+ManSzeLam109021153@users.noreply.github.com>
2022-11-24 17:49:07 +0800

    Add files via upload
    Signed-off-by: ManSzeLam109021153 <116985515+ManSzeLam109021153@users.noreply.github.com>

---
+++ University of Michigan/Programming for Everybody (Getting Started with Python)/Practise Programe in Python/Programming for Everybody_W6.py
@@ -0,0 +1,12 @@
+def computepay(h, e):
+    payment=(h*e)-(h-40)*e+((h-40)*e*1.5)
+    return payment
+h=float(input("Enter Hours:"))
+e=float(input("Enter Hours Pay:"))    
+
+if h <=40:
+    print("Hours Pay:",h*e)
+else:
+    payment=computepay(h,e)
+    print("Pay",payment)
+    
