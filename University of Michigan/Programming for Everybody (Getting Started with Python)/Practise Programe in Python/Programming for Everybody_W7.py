17084e8f12ffb1876ec453e269759f9b799f6215
ManSzeLam109021153 <116985515+ManSzeLam109021153@users.noreply.github.com>
2022-11-26 11:53:21 +0800

    Update Programming for Everybody_W7.py
    Signed-off-by: ManSzeLam109021153 <116985515+ManSzeLam109021153@users.noreply.github.com>

--- University of Michigan/Programming for Everybody (Getting Started with Python)/Practise Programe in Python/Programming for Everybody_W7.py
+++ University of Michigan/Programming for Everybody (Getting Started with Python)/Practise Programe in Python/Programming for Everybody_W7.py
@@ -1,17 +1,18 @@
 largest = None
 smallest = None
+all_numbers = []
 while True:
     num = input('Enter the Number:')
     if num == "done":
         break
     try:
-        user=int(num)
+        all_numbers.append(int(num))
     except:
         print('Invalid input')
         continue
-    else:
-        num=int(num)
-largest = max(num)
-smallest = min(num)
+#     else:
+#         num=int(num)
+largest = max(all_numbers)
+smallest = min(all_numbers)
 print("Maximum is", largest)
 print("Minimum is", smallest)
